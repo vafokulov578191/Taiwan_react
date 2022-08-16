18c2656a8d24cf234353144b02faeee4d4b8e780
Aminjon <vafokulov552@gmail.com>
2022-08-16 22:07:03 +0500

    arr

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
 
 
-function Main() {
+function Main({item}) {
+    let {name, buy, date} = item
   return (
     <main className="main flex flex-col gap-5 m-auto w-96 h-full">
         <div className="flex flex-wrap gap-5 items-center">
             <div className="item w-96 h-48 bg-item_color mb-5 rounded text-white flex flex-col justify-between p-5">
                 <div className="flex flex-col gap-5">
-                    <span className='font-semibold text-white text-lg'>Robinhood is limiting purchases of stocks: AMC, Blackberry, Nokia and GameStop</span>
+                    <span className='font-semibold text-white text-lg'>{name}</span>
                     <div className="flex justify-between text-gray-500">
-                        <span>By Miner49er</span>
+                        <span>{buy}</span>
                         <span className='underline'>Read More</span>
                     </div>
                 </div>
                 <div className="itembottom">
-                    <span className=' text-red-200'>10 Dec 2017</span>
+                    <span className=' text-red-200'>{date}</span>
                 </div>
             </div>
         </div>
